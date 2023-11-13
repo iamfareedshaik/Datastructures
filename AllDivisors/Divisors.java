3717f6cd850349babc8e1eaa6c44f7835f1feb4f
iamfareedshaik <iamfareedshaik@gmail.com>
2023-11-12 21:35:50 -0600

    All Divisors

---
+++ AllDivisors/Divisors.java
@@ -0,0 +1,20 @@
+class Divisors{
+    public static void checkDivisor(int n){
+        int i;
+        for(i =1;i*i<=n ;i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+        for( ; i>=1;i--){
+            if(n%i == 0){
+                System.out.println(n/i);
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int n = Integer.parseInt(args[0]);
+        checkDivisor(n);
+    }
+}
